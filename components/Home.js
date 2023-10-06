49e153908e3a73f2223f660adb81b2319e2946f2
Ruben <rubenhamou@gmail.com>
2023-10-06 17:04:47 +0200

    update home

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,25 @@
 import styles from '../styles/Home.module.css';
 import LastTweets from './LastTweets';
+import { useState } from 'react';
+import { Input, Button } from 'antd';
+
+
+const { TextArea } = Input;
 
 function Home() {
+  const [tweet, setTweet] = useState('');
+
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">HOME</a>
-        </h1>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', width: '30%' }}>
+          <TextArea
+              className={styles.textarea}
+              maxLength={280}
+              onChange={(e) => setTweet(e.target.value)} value={tweet} 
+          />
+          <Button type="primary">Tweet</Button>
+        </div>
         <LastTweets />
       </main>
     </div>
